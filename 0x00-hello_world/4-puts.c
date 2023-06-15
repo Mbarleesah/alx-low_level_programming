0bd4352ea11fe25c83847bf4fe6e22f86127a844
mbarleesah <palesasthembiso94@gmail.com>
2023-06-15 11:34:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include "holberton.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    _puts("Programming is like building a multilingual puzzle");
     return (0);
 }
